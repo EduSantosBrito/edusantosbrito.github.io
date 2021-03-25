98230fa4399d5d86f8860a615d170756d3a0bea3
EduSantosBrito <edu.santos.brito@gmail.com>
2021-03-25 16:08:46 -0300

    initial commit

---
+++ _next/static/ERgB3a0e9abe8Wh1Fro4L/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-8c9cde00a7bc05171bb4.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-81b0892f254f9808fec1.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
